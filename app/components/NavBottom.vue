f3f586c9db4fc10d1f57ead6adf5457e21a1ccf6
Virgill_e <virgile.bigare@gmail.com>
2025-10-21 16:29:02 +0200

    update navBottom

--- app/components/NavBottom.vue
+++ app/components/NavBottom.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="fixed bottom-0 left-0 right-0 w-full bg-black border-t border-gray-200 shadow-lg">
+    <nav class="fixed bottom-0 left-0 right-0 w-full bg-white/30 backdrop-blur-sm">
         <div class="flex flex-row items-center justify-evenly py-3">
             <button class="flex flex-col items-center justify-center p-2">
                 <img src="/img/home-icon.png" alt="icon" class="w-6 h-6" />
