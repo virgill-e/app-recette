aef26216ce079d0c80ce16f4ec9560a12f867b6b
Virgill_e <virgile.bigare@gmail.com>
2025-10-25 20:01:22 +0200

    componenet recipe card

---
+++ app/components/RecipeCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="h-[30vh] w-3/4 bg-white/40 rounded-xl">
+        <nuxt-img :src="imagePath" :alt="title" class="w-full h-3/4 rounded-xl object-cover" />
+        <div class="h-1/4 flex flex-col items-start justify-center">
+            <span class="block ml-4">{{ title }}</span>
+            <span class="block ml-4 text-sm">{{ chef }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    imagePath: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    chef: {
+        type: String,
+        required: true
+    }
+})
+</script>
--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="w-screen py-8 pb-20">
         <NavTop />
-        <div id="recipes" class="flex flex-col items-center justify-start my-16">
-            <div class="h-[30vh] w-3/4 bg-white/40 rounded-xl">
-                <nuxt-img src="/img/fake-img.jpg" alt="recette-1" class="w-full h-3/4 rounded-xl" />
-                <div class="h-1/4 flex flex-col items-start justify-center">
-                    <span class="block ml-4">titre</span>
-                    <span class="block ml-4">chef</span>
-                </div>
-            </div>
+        <div id="recipes" class="flex flex-col items-center justify-start my-16 gap-4">
+            <!-- RECIPE -->
+            <RecipeCard imagePath="/img/fake-img.jpg" title="titre" chef="chef" />
+            <RecipeCard imagePath="/img/fake-img.jpg" title="titre" chef="chef" />
+            <RecipeCard imagePath="/img/fake-img.jpg" title="titre" chef="chef" />
+            <RecipeCard imagePath="/img/fake-img.jpg" title="titre" chef="chef" />
+
         </div>
         <NavBottom />
     </div>
