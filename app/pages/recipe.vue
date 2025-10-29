3d43d8cc1052485ad37f318df4b8992834dc80ea
Virgill_e <virgile.bigare@gmail.com>
2025-10-29 21:40:27 +0100

    init page recipe

---
+++ app/pages/recipe.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="h-screen w-screen flex flex-col items-center justify-start">
+        <div>
+            présentation de la recette
+        </div>
+    </div>
+</template>
